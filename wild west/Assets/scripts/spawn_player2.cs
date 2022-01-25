a24792d5b9585c414d58bb90038c638130193d2a
coder1090 <97554327+coder1090@users.noreply.github.com>
2022-01-25 11:40:36 -0800

    0.2b

---
+++ wild west/Assets/scripts/spawn_player2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn_player2 : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 16;
+    private float spawnPosZ = -175;
+    private float startDelay = 1;
+    private float spawnInterval = 10000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            InvokeRepeating("SpawnRandomMonster", startDelay, spawnInterval);
+        }
+    }
+    void SpawnRandomMonster()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+
+    }
+}
