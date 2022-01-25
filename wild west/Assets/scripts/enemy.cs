13968285129371ca830abd6df4617b7f0e98518f
coder1090 <97554327+coder1090@users.noreply.github.com>
2022-01-25 14:15:38 -0800

    0.2.0c

---
+++ wild west/Assets/scripts/enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public float speed = 3.0f;
+    private Rigidbody EnemyRB;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemyRB = GetComponent<Rigidbody>();
+        player = GameObject.Find("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+
+        EnemyRB.AddForce(lookDirection * speed);
+    }
+}
+
