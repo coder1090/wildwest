1b0d8c22a745e09aa7af9aa82d9aaedadcd127ec
coder1090 <97554327+coder1090@users.noreply.github.com>
2022-03-09 16:43:03 -0800

    0.3.5.v

---
+++ wild west/Assets/scripts/start.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class start : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float zBound = -227.2f;
+    public float Zbound = -123.4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * -speed);
+
+        }
+
+        ConstrainPlayerPoistion();
+    }
+    void ConstrainPlayerPoistion()
+    {
+
+        if (transform.position.z < zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+        if (transform.position.z > Zbound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, Zbound);
+        }
+    }
+}
