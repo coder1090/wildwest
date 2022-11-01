a3fb39a38be63c83cb0411927367bf0a6de0c30e
coder1090 <97554327+coder1090@users.noreply.github.com>
2022-10-31 17:31:25 -0700

    0.6.0.v

---
+++ wild west/Assets/scripts/destroy_maps.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class destroy_maps : MonoBehaviour
+{
+    public Button west_button;
+    public Button wood_button;
+    public Button medow_button;
+    public Button snow_button;
+    public Button farm_button;
+    public Button cave_button;
+    public Button island_button;
+    public Button WhatCouldItBe_button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        west_button.onClick.AddListener(destroy);
+        wood_button.onClick.AddListener(destroy);
+        medow_button.onClick.AddListener(destroy);
+        snow_button.onClick.AddListener(destroy);
+        farm_button.onClick.AddListener(destroy);
+        cave_button.onClick.AddListener(destroy);
+        island_button.onClick.AddListener(destroy);
+        WhatCouldItBe_button.onClick.AddListener(destroy);
+
+    }
+    void destroy()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ wild west/Assets/scripts/spawn_west.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class spawn_west : MonoBehaviour
+{
+    public GameObject[] west;
+    public float westX = 7.3f;
+    public float westZ = -98.0f;
+    public Button westButon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        westButon.onClick.AddListener(westSpwan);
+    }
+    public void westSpwan(){
+        int westIndex = Random.Range(0, west.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(westX, westX), 0, westZ);
+
+        Instantiate(west[westIndex], spawnPos, west[westIndex].transform.rotation);
+    }
+}
