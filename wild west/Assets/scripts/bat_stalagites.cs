12a73c7e376f26111aeb8889aaaaeb0ea2648aaa
coder1090 <97554327+coder1090@users.noreply.github.com>
2022-12-12 10:21:53 -0800

    0.6.6.v

---
+++ wild west/Assets/scripts/bat_stalagites.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bat_stalagites : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    public float spawnrangex = 0;
+    public float spawnangex2 = 1;
+    public float spawnrangeY = -8;
+    public float spawnposz = -18;
+    public float startDelay = 12;
+    public float spawnInterval = 10.0f;
+    public bool space = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && space)
+        {
+            InvokeRepeating("SpawnRandomMonster", startDelay, spawnInterval);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            space = false;
+        }
+    }
+    void SpawnRandomMonster()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnrangex, spawnangex2), spawnrangeY, spawnposz);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+
+    }
+}
